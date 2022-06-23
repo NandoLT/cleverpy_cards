9b5a58cbf349d96ba0ec15f1955ae162bf7a65f6
Fernando Lopez Trejo <fernando_lt@hotmail.es>
2022-06-23 18:02:29 +0200

    Implement Loader component

--- src/components/Pages/Home/CardsContainer.tsx
+++ src/components/Pages/Home/CardsContainer.tsx
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import { useEffect } from 'react';
 import { useAppSelector, useAppDispatch } from '../../../App/hooks';
 import { fetchPosts, selectPostsListData } from '../../../features/posts/postsSlice';
 import { Card } from './Card';
+import Loader from '../../commons/Loader';
 
 import '../../../assets/css/Card.css';
 
@@ -10,13 +12,31 @@ export const CardsContainer = () => {
   let posts = useAppSelector(selectPostsListData);
   const dispatch = useAppDispatch();
 
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
-    dispatch(fetchPosts())
+    setIsLoading(true);
+    dispatch(fetchPosts());
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1500)
   }, [dispatch])
 
   return (
     <>
+        { isLoading ? <Loader />
+          :        
       <div className='card-container'>
+          {posts.length > 0 ?
+            <Card postsInfo={posts}/>
+            :
+            <div>
+              <h2>No Post availables</h2>
+            </div>}
+      </div>
+        }
+      {/* <div className='card-container'>
+        { isLoading && <Loader />}
         {
           posts.length > 0 ?
             <Card postsInfo={posts}/>
@@ -25,7 +45,7 @@ export const CardsContainer = () => {
               <h2>No Post availables</h2>
             </div>
         }
-      </div>
+      </div> */}
     </>
   )
 }
